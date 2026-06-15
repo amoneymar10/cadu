3737694590ffaca83c2080885c85823d85fb1e6b
amoneymar10 <carlos.gomes.barbosa@escola.pr.gov.br>
2026-06-14 22:38:56 -0300

    antes e depois parte 1

--- main.js
+++ main.js
@@ -132,4 +132,14 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
+    const comparativoToggle = document.getElementById('comparativo-toggle');
+    const comparativoGrid = document.getElementById('comparativo-grid');
+
+    if (comparativoToggle && comparativoGrid) {
+        comparativoToggle.addEventListener('click', () => {
+            const estaMostrandoDepois = comparativoGrid.classList.toggle('mostrar-depois');
+            comparativoToggle.innerText = estaMostrandoDepois ? 'Mostrar comparativo completo' : 'Mostrar apenas o depois';
+        });
+    }
+
 }); // Fim do DOMContentLoaded
